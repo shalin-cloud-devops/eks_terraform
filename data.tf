08f5e5958a5336e12eda376970e18685a8bec25d
shalin-cloud-devops <shalin.citi.devops@gmail.com>
2026-07-03 19:15:57 +0530

    CI pipeline added

--- data.tf
+++ data.tf
@@ -5,7 +5,7 @@ data "aws_ami" "ubuntu" {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
-
+  #Dummy COmmit
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
